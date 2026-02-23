a763e57741770e1b87111c324294393d682ac6f0
Vivo2005 <lvm1@kent.ac.uk>
2026-02-23 11:46:06 +0000

    validator works

--- validator.py
+++ validator.py
@@ -1,3 +1,4 @@
+import csv
 import re
 
 def is_valid_email (email):
@@ -7,8 +8,12 @@ def is_valid_email (email):
 
 def main():
     email= input("What's your email?").strip()
+    name= input("whats your name?").strip()
     if is_valid_email(email):
-        print("valid")
+        print("valid email, saving to contacts...")
+        with open("contacts.csv","a") as file:
+            writer =  csv.DictWriter (file, fieldnames = ["name","email"])
+            writer.writerow({"name":name, "email":email})
     else :
         print("invalid")
 
